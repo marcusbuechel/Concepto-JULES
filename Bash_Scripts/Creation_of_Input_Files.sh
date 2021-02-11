4a6966e318c917cfd42a15d7300a2306486170b3
marcusbuechel <marcus.buechel@chch.ox.ac.uk>
2021-02-11 12:55:14 +0000

    Base variable input and log creation

--- Bash_Scripts/Creation_of_Input_Files.sh
+++ Bash_Scripts/Creation_of_Input_Files.sh
@@ -34,7 +34,9 @@ possible!"
 #Get the initial time for when this script began running
 time1=$(date)
 
-
+#Create the log for this script
+echo -e "The Second Concepto-JULES bash script has begun running... \nfingers crossed it finishes!" >> ${base_save}Concepto-JULES/Logs/Log_Two.txt
+echo  "Script started at: " ${time1} >> ${base_save}Concepto-JULES/Logs/Log_Two.txt
 
 #Set the base directory
 base_save=/work/scratch-pw/$USER/Concepto-JULES/
@@ -42,8 +44,34 @@ base_save=/work/scratch-pw/$USER/Concepto-JULES/
 #Set the meteorological data file
 meteo_data=/group_workspaces/jasmin2/jules_bd/data/CHESS_v1.2/met_uncompressed/
 
+echo "Getting some files for setup now..."
+
 #Get the text catchment shapefile
-wget -r -np -nd -P /work/scratch-pw/mehb/Concepto-JULES/Input/Driving_Data/ https://github.com/marcusbuechel/Concepto-JULES/blob/main/Test_Data/Tamar/47001.zip
+wget -r -np -nd -P /work/scratch-pw/mehb/Concepto-JULES/Input/Driving_Data/ https://github.com/marcusbuechel/Concepto-JULES/blob/main/Test_Data/Tamar/47001.zip >> ${base_save}Concepto-JULES/Logs/Log_Two.txt
 
 #Unzip the files
-unzip /work/scratch-pw/mehb/Concepto-JULES/Input/Driving_Data/47001.zip -d /work/scratch-pw/mehb/Concepto-JULES/Input/Driving_Data/
+unzip /work/scratch-pw/mehb/Concepto-JULES/Input/Driving_Data/47001.zip -d /work/scratch-pw/mehb/Concepto-JULES/Input/Driving_Data/ >> ${base_save}Concepto-JULES/Logs/Log_Two.txt
+
+#Load in the module JASPY on JASMIN to make the catchment averaged files
+module load jaspy
+
+#Input variables
+year1=2000 >> ${base_save}Concepto-JULES/Logs/Log_Two.txt
+year2=2001 >> ${base_save}Concepto-JULES/Logs/Log_Two.txt
+years=($(seq $year1 $year2))
+echo "Years to plot are: " >> ${base_save}Concepto-JULES/Logs/Log_Two.txt
+for year in ${years[@]}
+do
+  echo "Year selected for enquiry: " $year >> ${base_save}Concepto-JULES/Logs/Log_Two.txt
+done
+catchment="47001" >> ${base_save}Concepto-JULES/Logs/Log_Two.txt
+
+#Meteo variables to run JULES
+meteo=(rsds rlds precip tas sfcWind huss dtr psurf)
+echo "Meteo data to reduce: " >> ${base_save}Concepto-JULES/Logs/Log_Two.txt
+for var in ${meteo[@]}
+do
+  echo $var >> ${base_save}Concepto-JULES/Logs/Log_Two.txt
+done
+
+echo "Base variables have been input..."
