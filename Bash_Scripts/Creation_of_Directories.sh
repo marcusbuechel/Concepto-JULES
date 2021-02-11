94991cdb01407fb44953ae7c72f0393afad530dd
marcusbuechel <marcus.buechel@chch.ox.ac.uk>
2021-02-11 10:24:48 +0000

    Create Creation_of_Directories bash script
    This script is the first script to create the directories that we will be working with for the rest of the Concepto-JULES process.

---
+++ Bash_Scripts/Creation_of_Directories.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+################################################################################
+#   _____                           _
+#  / ____|                         | |
+# | |     ___  _ __   ___ ___ _ __ | |_ ___    ______
+# | |    / _ \| '_ \ / __/ _ \ '_ \| __/ _ \  |______|
+# | |___| (_) | | | | (_|  __/ |_) | || (_) |
+#  \_____\___/|_| |_|\___\___| .__/ \__\___/
+#                            | |
+#                            |_|
+#        _ _    _ _      ______  _____
+#       | | |  | | |    |  ____|/ ____|
+#       | | |  | | |    | |__  | (___
+#   _   | | |  | | |    |  __|  \___ \
+#  | |__| | |__| | |____| |____ ____) |
+#   \____/ \____/|______|______|_____/
+#
+################################################################################
+# Author: Marcus Buechel
+# Contact: marcus.buechel@ouce.ox.ac.uk
+
+# Description
+# This is the first part of the bash scripts used to run distributed catchments
+# as a single point in JULES. In this script, the folder structure is created
+# for the user to create a Concepto-JULES run.
+
+# NOTE: This is meant for running on the JASMIN system and you need to be logged
+# onto the cylc.jasmin.ac.uk server.
+
+#Get the initial time for when this script began running
+time1= date
+
+#Set the base directory
+base_save=/work/scratch-pw/$USER/
+
+#Make the directories
+mkdir -p ${base_save}Concepto-JULES/Input/Driving_Data
+mkdir ${base_save}Concepto-JULES/Input/JULES
+mkdir -p ${base_save}Concepto-JULES/Output/JULES_Data
+mkdir ${base_save}Concepto-JULES/Output/Refined_Data
+mkdir ${base_save}Concepto-JULES/Logs
+
+#Make the first log file for the work
+echo -e "The First Concepto-JULES bash script has begun running... \nfingers crossed it finishes!" >> ${base_save}Concepto-JULES/Logs/Log_One.txt
+echo  "Script started at " + time1 >> ${base_save}Concepto-JULES/Logs/Log_One.txt
+
+#Show final time
+time2= date
+
+echo  "Script finished at " + time2 >> ${base_save}Concepto-JULES/Logs/Log_One.txt
