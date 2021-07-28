f206b131b8adec97f2bc62e64f2691e3993b5bad
marcusbuechel <47666094+marcusbuechel@users.noreply.github.com>
2021-07-28 14:45:40 +0100

    New R Script to Average Netcdf files

---
+++ Code/R_Averaging_Area.R
@@ -0,0 +1,31 @@
+################################################################################
+#   _____                           _
+#  / ____|                         | |
+# | |     ___  _ __   ___ ___ _ __ | |_ ___    ______
+# | |    / _ \| '_ \ / __/ _ \ '_ \| __/ _ \  |______|
+# | |___| (_) | | | | (_|  __/ |_) | || (_) |
+#  \_____\___/|_| |_|\___\___| .__/ \__\___/
+#                            | |
+#                            |_|
+#        _ _    _ _      ______  _____
+#       | | |  | | |    |  ____|/ ____|
+#       | | |  | | |    | |__  | (___
+#   _   | | |  | | |    |  __|  \___ \
+#  | |__| | |__| | |____| |____ ____) |
+#   \____/ \____/|______|______|_____/
+#
+################################################################################
+# Author: Marcus Buechel
+# Contact: marcus.buechel@ouce.ox.ac.uk
+
+# Description
+# This R script is used to average the variables in a shapefile area to run with
+# Concepto-JULES.
+
+# NOTE: This is meant for running on the JASMIN system and you need to be logged
+# onto the cylc.jasmin.ac.uk server.
+
+
+# Load in the libraries ---------------------------------------------------
+
+
