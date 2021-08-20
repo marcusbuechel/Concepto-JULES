0962ed4e72ecd6362cfe55c3b0617f19a808ca43
marcusbuechel <47666094+marcusbuechel@users.noreply.github.com>
2021-08-20 17:38:42 +0100

    Creation of the python script to average variables

---
+++ Code/Reproject_Shapefile_CJ.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 20 16:46:28 2021
+
+@author: marcu
+"""
+
+#Reproject shapefile
+
+import sys
+import os
+import geopandas
+
+
+path= sys.argv[1]
+file_in= '47001.shp'
+file_out='reproj_47001.shp'
+data = geopandas.read_file(path+file_in)
+# change CRS to epsg 4326
+data = data.to_crs(epsg=4277)
+# write shp file
+data.to_file(path+file_out)
